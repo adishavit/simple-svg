e60a63708dcba70e27ac329feaf3deb5622b40b8
Justin K -W10 <justinjoseph0007@gmail.com>
2017-10-22 16:12:21 -0400

    Fixed linker errors when used in big projects by inlining functions

--- simple_svg_1.0.0.hpp
+++ simple_svg_1.0.0.hpp
@@ -1,4 +1,3 @@
-
 /*******************************************************************************
 *  The "New BSD License" : http://www.opensource.org/licenses/bsd-license.php  *
 ********************************************************************************
@@ -44,22 +43,22 @@ namespace svg
 {
     // Utility XML/String Functions.
     template <typename T>
-    std::string attribute(std::string const & attribute_name,
+    inline std::string attribute(std::string const & attribute_name,
         T const & value, std::string const & unit = "")
     {
         std::stringstream ss;
         ss << attribute_name << "=\"" << value << unit << "\" ";
         return ss.str();
     }
-    std::string elemStart(std::string const & element_name)
+    inline std::string elemStart(std::string const & element_name)
     {
         return "\t<" + element_name + " ";
     }
-    std::string elemEnd(std::string const & element_name)
+    inline std::string elemEnd(std::string const & element_name)
     {
         return "</" + element_name + ">\n";
     }
-    std::string emptyElemEnd()
+    inline std::string emptyElemEnd()
     {
         return "/>\n";
     }
@@ -103,7 +102,7 @@ namespace svg
         double x;
         double y;
     };
-    optional<Point> getMinPoint(std::vector<Point> const & points)
+    inline optional<Point> getMinPoint(std::vector<Point> const & points)
     {
         if (points.empty())
             return optional<Point>();
@@ -117,7 +116,7 @@ namespace svg
         }
         return optional<Point>(min);
     }
-    optional<Point> getMaxPoint(std::vector<Point> const & points)
+    inline optional<Point> getMaxPoint(std::vector<Point> const & points)
     {
         if (points.empty())
             return optional<Point>();
@@ -147,7 +146,7 @@ namespace svg
     };
 
     // Convert coordinates in user space to SVG native space.
-    double translateX(double x, Layout const & layout)
+    inline double translateX(double x, Layout const & layout)
     {
         if (layout.origin == Layout::BottomRight || layout.origin == Layout::TopRight)
             return layout.dimensions.width - ((x + layout.origin_offset.x) * layout.scale);
@@ -155,14 +154,14 @@ namespace svg
             return (layout.origin_offset.x + x) * layout.scale;
     }
 
-    double translateY(double y, Layout const & layout)
+    inline double translateY(double y, Layout const & layout)
     {
         if (layout.origin == Layout::BottomLeft || layout.origin == Layout::BottomRight)
             return layout.dimensions.height - ((y + layout.origin_offset.y) * layout.scale);
         else
             return (layout.origin_offset.y + y) * layout.scale;
     }
-    double translateScale(double dimension, Layout const & layout)
+    inline double translateScale(double dimension, Layout const & layout)
     {
         return dimension * layout.scale;
     }
@@ -296,7 +295,7 @@ namespace svg
         Stroke stroke;
     };
     template <typename T>
-    std::string vectorToString(std::vector<T> collection, Layout const & layout)
+    inline std::string vectorToString(std::vector<T> collection, Layout const & layout)
     {
         std::string combination_str;
         for (unsigned i = 0; i < collection.size(); ++i)
